9c381aa5564dedb01d81c551c796883fedfb2704
Zack Hartmann <zackhartmann@gmail.com>
2017-12-04 18:26:02 -0500

    HAP Package Installed

---
+++ WebScaper/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+
+namespace WebScaper
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //HTML AGILITy PACK
+            //scrapysharp
+//            var headerNames - DoubleConverter.DocumentNode.Selectnodes("//table[@class='bars']");
+        }
+    }
+}

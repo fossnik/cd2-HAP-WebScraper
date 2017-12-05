ee46e525d0f01d05291722ca0e3a66e99dd43b2e
Zack Hartmann <zackhartmann@gmail.com>
2017-12-04 19:57:10 -0500

    Removed Unneeded Dependencies

--- WebScaper/Program.cs
+++ WebScaper/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text.RegularExpressions;
 using HtmlAgilityPack;
 
 namespace WebScaper

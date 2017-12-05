a22b5fa13f5a9989a0a16f08578f485b9b891c9d
Zack Hartmann <zackhartmann@gmail.com>
2017-12-04 19:35:53 -0500

    Outputs Document from File

--- WebScaper/Program.cs
+++ WebScaper/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using HtmlAgilityPack;
 
@@ -11,13 +12,24 @@ namespace WebScaper
     {
         public static void Main(string[] args)
         {
-            HtmlWeb htmlWeb = new HtmlWeb();
-            HtmlDocument htmlDocument = htmlWeb.Load("https://news.ycombinator.com/");
+//            HtmlWeb htmlWeb = new HtmlWeb();
+//            HtmlDocument htmlDocument = htmlWeb.Load("https://news.ycombinator.com/");
+
+            const string filePath = @"/home/seth/careerDevs-i/RiderProjects/WebScaper/HackerNews.htm";
+            HtmlDocument htmlDocument = new HtmlDocument();
+            htmlDocument.Load(filePath);
             
-            HtmlWeb htmlWeb = new HtmlWeb() {
+            if (htmlDocument.DocumentNode == null)
+                Console.WriteLine("null document node!");
             
-            //scrapysharp
-//            var headerNames - DoubleConverter.DocumentNode.Selectnodes("//table[@class='bars']");
+            Console.WriteLine(htmlDocument.DocumentNode.InnerHtml);
+
+//            var articles = htmlDocument.DocumentNode.SelectNodes("//a[@class='storylink']//a");
+//
+//            foreach (HtmlNode node in articles)
+//            {
+//                Console.WriteLine(node.ChildNodes[0].InnerHtml);
+//            }
         }
     }
 }
